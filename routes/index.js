f23f0ed7bf1ba14785e534d7c8417e3938e132f3
Cristian Gutierrez <cristiandisenadorvisual@gmail.com>
2024-06-26 18:31:39 -0500

    Se agrega validacion para el PUT

--- routes/index.js
+++ routes/index.js
@@ -35,9 +35,46 @@ router.post("/dogs", (req, res) => {
 });
 
 router.put("/dogs/:dogid", (req, res) => {
+  const { name, breed, image, color, age, favoriteToy, personality, bio } =
+    req.body;
+  let data = {};
+
+  if (age !== undefined && age !== null && age !== "") {
+    try {
+      const parsedValue = parseInt(age);
+      if (isNaN(parsedValue)) {
+        res.status(304).send("Invalid number");
+      }
+      data["age"] = parsedValue;
+    } catch (error) {
+      res.status(304).send(error);
+    }
+  }
+  if (name !== undefined && name !== null && name !== "") {
+    data["name"] = name;
+  }
+  if (breed !== undefined && breed !== null && breed !== "") {
+    data["breed"] = breed;
+  }
+  if (image !== undefined && image !== null && image !== "") {
+    data["image"] = image;
+  }
+  if (color !== undefined && color !== null && color !== "") {
+    data["color"] = color;
+  }
+  if (favoriteToy !== undefined && favoriteToy !== null && favoriteToy !== "") {
+    data["favoriteToy"] = favoriteToy;
+  }
+  if (personality !== undefined && personality !== null && personality !== "") {
+    data["personality"] = personality;
+  }
+  if (bio !== undefined && bio !== null && bio !== "") {
+    data["bio"] = bio;
+  }
+
   Dog.findById(req.params.dogid)
     .then((dbdogid) => {
-      const newDogData = Object.assign(dbdogid, req.body);
+      const newDogData = Object.assign(dbdogid, data);
       return newDogData.save();
     })
     .then((result) => {
